cf1ec0e079491781b448e7a28c52cf46ed58022e
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-06-02 03:02:59 +0530

    feat: add support for reverse geocoding

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,6 @@
 class Location < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode, if: :latitude_changed? || :longitude_changed?
 end
