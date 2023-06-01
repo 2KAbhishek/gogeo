49c288a130e5bfc56c92c78a9e099978e6e8276f
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-06-02 02:39:54 +0530

    feat: add search support to controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,21 @@
+# frozen_string_literal: true
+
+# Location Controller
 class LocationsController < ApplicationController
-  before_action :set_location, only: %i[ show edit update destroy ]
+  before_action :set_location, only: %i[show edit update destroy]
 
   # GET /locations
   def index
-    @locations = Location.all
+    @locations =
+      if params[:search].present?
+        Location.near(params[:search], 200, order: :distance)
+      else
+        Location.all
+      end
   end
 
   # GET /locations/1
-  def show
-  end
+  def show; end
 
   # GET /locations/new
   def new
@@ -16,15 +23,14 @@ class LocationsController < ApplicationController
   end
 
   # GET /locations/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /locations
   def create
     @location = Location.new(location_params)
 
     if @location.save
-      redirect_to @location, notice: "Location was successfully created."
+      redirect_to @location, notice: 'Location was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +39,7 @@ class LocationsController < ApplicationController
   # PATCH/PUT /locations/1
   def update
     if @location.update(location_params)
-      redirect_to @location, notice: "Location was successfully updated."
+      redirect_to @location, notice: 'Location was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +48,18 @@ class LocationsController < ApplicationController
   # DELETE /locations/1
   def destroy
     @location.destroy
-    redirect_to locations_url, notice: "Location was successfully destroyed.", status: :see_other
+    redirect_to locations_url, notice: 'Location was successfully destroyed.', status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_location
-      @location = Location.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def location_params
-      params.require(:location).permit(:address, :latitude, :longitude)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def location_params
+    params.require(:location).permit(:address, :latitude, :longitude)
+  end
 end
